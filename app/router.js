3d8d42534f758a6fca58c3f36e1d389b80f7d260
Tao Ning <tning@linkedin.com>
2018-01-29 00:40:05 -0500

    Add routes

--- app/router.js
+++ app/router.js
@@ -6,6 +6,41 @@ const Router = EmberRouter.extend({
   rootURL: config.rootURL,
 });
 
-Router.map(function() {});
+Router.map(function() {
+  this.route(
+    'authentication',
+    {
+      path: '/',
+      resetNamespace: true,
+    },
+    function() {
+      this.route(
+        'post',
+        {
+          path: '/',
+          resetNamespace: true,
+        },
+        function() {
+          this.route('new', function() {
+            this.route('drafts');
+            this.route('published');
+          });
+
+          this.route('edit', function() {
+            this.route('index', { path: '/:post_id' });
+            this.route('drafts');
+            this.route('published');
+          });
+
+          this.route('redirect', { path: '/*wildcard' });
+        }
+      );
+    }
+  );
+
+  this.route('logout', { resetNamespace: true });
+
+  this.route('login', { resetNamespace: true });
+});
 
 export default Router;
---
+++ tests/unit/routes/authentication/post/edit/drafts-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:authentication/post/edit/drafts', 'Unit | Route | authentication/post/edit/drafts', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
