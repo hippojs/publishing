a5041bcaa7ca8820556630e9dadd95fca6d2cdf1
Tao Ning <tning@linkedin.com>
2018-01-29 01:15:50 -0500

    Implement basic save action

--- app/components/quill-editor.js
+++ app/components/quill-editor.js
@@ -3,7 +3,22 @@ import Quill from 'Quill';
 
 export default Component.extend({
   didInsertElement() {
-    debugger;
-    console.log('insert me');
+    this.quill = new Quill('#editor', {
+      modules: {
+        toolbar: [
+          [{ header: [1, 2, false] }],
+          ['bold', 'italic', 'underline'],
+          ['image', 'code-block'],
+        ],
+      },
+      placeholder: 'Compose an epic...',
+      theme: 'snow',
+    });
+  },
+  actions: {
+    save() {
+      const content = this.quill.getContents();
+      this.get('onSave')(content);
+    },
   },
 });
--- app/routes/post.js
+++ app/routes/post.js
@@ -1,4 +1,14 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model() {
+    return {
+      currentPost: {},
+    };
+  },
+  actions: {
+    save(content) {
+      console.log(`press save, content: ${JSON.stringify(content)}`);
+    },
+  },
 });
