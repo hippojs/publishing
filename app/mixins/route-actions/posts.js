2bbca5aacb9a83553d1a6cad94fc9638ce85c7f2
hippojs <hippojs.guo@gmail.com>
2018-02-02 07:12:07 -0500

    Add nav-bar and basic post action in it

--- app/mixins/route-actions/posts.js
+++ app/mixins/route-actions/posts.js
@@ -3,14 +3,26 @@ import { ROUTES } from 'publishing/constants/global';
 
 export default Mixin.create({
   actions: {
+    publishPost(model) {
+      console.log('call from mixin: publishPost');
+
+      return model
+        .save()
+        .then(model => {
+          console.log('call from mixin: model saved');
+          // TODO: transition to pulse/detail
+          // this.transitionTo(ROUTES.POST_EDIT, model.get('id'))
+        })
+        .catch(err => console.log(err));
+    },
+
     /**
      * Actually updates the post content and headline, does validation as well
      * TODO validation
      * @param  {string} The post content (alias: contentHtml)
      */
     updatePost(model, key, value = '') {
-      console.log(`call from mixin model: ${model}`);
-      console.log(`call from mixin model: ${value}`);
+      console.log('call from mixin: updatePost');
 
       if (!key || !value) {
         return;
