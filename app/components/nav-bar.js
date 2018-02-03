825a61c44ffaa60bcfa964a0a327161397d680c9
hippojs <hippojs.guo@gmail.com>
2018-02-03 18:58:48 -0500

    Create publish-modal, move publish action into it

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -1,22 +1,3 @@
 import Component from '@ember/component';
 
-export default Component.extend({
-  actions: {
-    publish() {
-      // TODO: refactor valide to a util, also update it in `quill-editor`
-      // if user haven't type anything, quill-editor not set content => content
-      // is null
-      // if user type, then delete, there is still a blank line represented by
-      // ‘\n’, so getLength will return 1.
-      if (
-        !this.get('model.content.length') ||
-        // FIX: need to figure out a way to get currentContent from quill-editor
-        this.get('model.content.length') === 1
-      ) {
-        console.log("error: don't have content");
-        return;
-      }
-      this.get('onPublish')(this.model);
-    },
-  },
-});
+export default Component.extend({});
--- app/components/publishing-page.js
+++ app/components/publishing-page.js
@@ -1,10 +1,25 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  init() {
+    this._super(...arguments);
+    this.setProperties({
+      showPublishModal: false,
+    });
+  },
   // TODO:
   //   1. Set valide title && CP
   //   2. Pass it to Nav-bar
   setContentValid(isValid) {
     this.get('currentPost').set('hasValidContent', isValid);
+  },
+  actions: {
+    openPublishModal() {
+      // TODO: validate title / content, if not ok, don't set following to true
+      this.set('showPublishModal', true);
+    },
+    closePublishModal() {
+      this.set('showPublishModal', false);
+    },
   },
 });
